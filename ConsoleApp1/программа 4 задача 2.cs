45950c28dbfaca6a3bd7c6574d5c6a04fc716f20
Никита Олегович Романенко <nikcom526@mail.ru>
2023-12-31 14:39:00 +0300

    программа 4

---
+++ ConsoleApp1/программа 4 задача 2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;
+            var pr = 1;
+            int buf;
+            do
+            {
+                buf = int.Parse(Console.ReadLine());
+                if (buf > 0)
+                    sum += buf;
+                else
+                    if (buf != 0)
+                    pr *= buf;
+            }
+            while (buf != 0);
+            if (sum > pr)
+                Console.WriteLine("Сумма ({0}) больше произведения ({1})", sum, pr);
+            else
+                if (sum < pr)
+                Console.WriteLine("Произведение ({1}) больше суммы ({0})", sum, pr);
+            else
+                Console.WriteLine("Произведение ({1}) и сумма ({0}) равны", sum, pr);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp2/программа 4 задача 3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите A: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите B: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите C: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int k = 0;
+            int n = 0;
+            while (a >= c)
+            {
+                a -= c;
+                k++;
+                while (b >= c)
+                {
+                    b -= c;
+                    n++;
+                }
+                int count = 0;
+                for (int i = 0; i < k; i++)
+                    count += n;
+                Console.WriteLine("N= " + count);
+            }
+        } 
+    }
+}
