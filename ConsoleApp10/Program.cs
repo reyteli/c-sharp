1931a2009a3b1e9d1392e28dd61bc1455b565f94
Никита Олегович Романенко <nikcom526@mail.ru>
2023-12-31 14:18:37 +0300

    программа

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -6,7 +6,17 @@ namespace ConsoleApp10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("введите число N");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int s = 0;
+            for (int i = 1; i <= N ; i++)
+            {
+                s += 2 * i - 1;
+                
+            }
+            Console.WriteLine( s);
+            Console.ReadKey();
         }
     }
 }
+
